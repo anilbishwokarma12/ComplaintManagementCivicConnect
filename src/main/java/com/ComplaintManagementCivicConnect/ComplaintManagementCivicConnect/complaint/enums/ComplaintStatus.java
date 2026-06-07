8444624ec1676e6e438284f04e6536067b8885cc
Anil <anilbishwokarma16>
2026-06-07 13:36:42 +0545

    added

---
+++ src/main/java/com/ComplaintManagementCivicConnect/ComplaintManagementCivicConnect/complaint/enums/ComplaintStatus.java
@@ -0,0 +1,10 @@
+package com.ComplaintManagementCivicConnect.ComplaintManagementCivicConnect.complaint.enums;
+
+public enum ComplaintStatus {
+
+    PENDING_REVIEW,
+    ASSIGNED,
+    IN_PROGRESS,
+    COMPLETED,
+    REJECTED
+}
